c31fab83e1c08381babbe2d18c3d5f87727f94b5
Clément Taboulot <clement-taboulot@hotmail.fr>
2024-09-10 12:23:58 +0200

    generate json from json with real value

--- react-editor/src/main.ts
+++ react-editor/src/main.ts
@@ -1,8 +1,81 @@
 import { emit, on, showUI } from "@create-figma-plugin/utilities";
 import merge from "lodash.merge";
+import rgbHex from "rgb-hex";
 
-function arrayToNestedObject(arr: string[]) {
-  return arr.reduceRight((acc, curr) => ({ [curr]: acc }), {});
+function arrayToNestedObject(arr: string[], value: any) {
+  return arr.reduceRight((acc, curr) => ({ [curr]: acc }), value);
+}
+
+function isVariableAlias(variable: VariableValue): variable is VariableAlias {
+  return (variable as VariableAlias).id !== undefined;
+}
+
+function isVariableRGB(variable: VariableValue): variable is RGB {
+  return (variable as RGBA).a === undefined;
+}
+
+function isVariableRGBA(variable: VariableValue): variable is RGBA {
+  return (variable as RGBA).a !== undefined;
+}
+
+function findVariableValue(
+  variableValue: VariableValue,
+  variables: Variable[]
+) {
+  let currentVariableValue = variableValue;
+  while (isVariableAlias(currentVariableValue)) {
+    const variableId = currentVariableValue.id;
+    const variableFound = variables.find(
+      (variable) => variable.id === variableId
+    );
+    if (!variableFound) {
+      throw Error(`Variable ID ${variableId} was not found`);
+    }
+
+    currentVariableValue =
+      variableFound.valuesByMode[Object.keys(variableFound.valuesByMode)[0]];
+  }
+
+  return currentVariableValue;
+}
+
+function convertVariableValue(variableValue: VariableValue) {
+  if (typeof variableValue === "string") {
+    return variableValue;
+  }
+
+  if (typeof variableValue === "number") {
+    return variableValue;
+  }
+
+  if (typeof variableValue === "boolean") {
+    return variableValue;
+  }
+
+  if (isVariableRGB(variableValue)) {
+    return (
+      "#" +
+      rgbHex(
+        variableValue.r * 255,
+        variableValue.g * 255,
+        variableValue.b * 255
+      )
+    );
+  }
+
+  if (isVariableRGBA(variableValue)) {
+    return (
+      "#" +
+      rgbHex(
+        variableValue.r * 255,
+        variableValue.g * 255,
+        variableValue.b * 255,
+        variableValue.a
+      )
+    );
+  }
+
+  throw Error(`Variable value type not supported: ${variableValue}`);
 }
 
 export default function () {
@@ -14,13 +87,22 @@ export default function () {
 
     variables.forEach((variable) => {
       const path = variable.name.split("/");
+      // TODO: take the value depending on collections modes
+      const rawValue =
+        variable.valuesByMode[Object.keys(variable.valuesByMode)[0]];
+      const variableValue = findVariableValue(rawValue, variables);
+      console.log("variableValue", variableValue);
+      console.log("path", path);
 
-      const object = arrayToNestedObject(path);
+      const object = arrayToNestedObject(
+        path,
+        convertVariableValue(variableValue)
+      );
       jsonVariables = merge(jsonVariables, object);
     });
 
     emit("CONVERTION_DONE", jsonVariables);
   });
 
-  showUI({ height: 240, width: 320, themeColors: false });
+  showUI({ height: 1000, width: 320, themeColors: false });
 }

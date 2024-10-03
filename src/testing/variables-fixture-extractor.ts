1111dc4d755afd5446fd57de16606434c6a3f3ed
Pierre Poupin <pierrep@bam.tech>
2024-10-03 14:17:33 +0200

    chore: add variable extractor

---
+++ src/testing/variables-fixture-extractor.ts
@@ -0,0 +1,17 @@
+/**
+ * Use this function to print the values of variables
+ * It might get handy again as I didn't export all the variable properties
+ * 
+ * Without this function, all properties except for the id are omitted :'(
+ */
+const convertVariablesToCopiableString = (variables) => {
+  const converted = variables.map((variable) => {
+    return {
+      id: variable.id,
+      name: variable.name,
+      valuesByMode: variable.valuesByMode,
+    };
+  });
+
+  console.log("converted", JSON.stringify(converted));
+};

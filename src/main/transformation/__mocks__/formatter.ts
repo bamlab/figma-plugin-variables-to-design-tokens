695b9f9fef98812bc6de06f52c1d09978eef2562
Pierre Poupin <pierrep@bam.tech>
2024-10-07 11:50:12 +0200

    tests: use typescript parser for prettier

--- src/main/transformation/__mocks__/formatter.ts
+++ src/main/transformation/__mocks__/formatter.ts
@@ -1,4 +1,4 @@
 import * as prettier from "prettier";
 
-export const formatCode = (inputCode: string): string => prettier.format(inputCode, { parser: "babel" });
+export const formatCode = (inputCode: string): string => prettier.format(inputCode, { parser: "typescript" });
 ;
